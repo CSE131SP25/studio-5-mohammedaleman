e0ad524a2601e3f527aba61ef2f4f5b63954e0e6
rubyaleman-rojas <rubyaleman-rojas@localhost>
2025-02-26 17:20:12 -0600

    finished studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,7 +16,10 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		double squareX = (x2-x1)*(x2-x1);
+		double squareY = (y2-y1)*(y2-y1);
+		distance = Math.sqrt(squareX+ squareY);
+		System.out.println(distance);
 		return distance;
 	}
 
@@ -28,22 +31,36 @@ public class Methods {
 	 * @param radius the radius of the bull's eye
 	 */
 	public static void drawBullsEye(double x, double y, double radius) {
+		
+	
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
-
+	
+		
+        
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0,109,219);
+		double radiusBlue= (3.0/4.0 * radius) ;
+		StdDraw.filledCircle(x, y, radiusBlue);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146,0,0);
+		double radiusRed= (1.0/2.0) * radius;
+		StdDraw.filledCircle(x, y, radiusRed);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
+		
+		StdDraw.setPenColor(255,255,109);
+		double radiusYellow= (1.0/4.0) * radius;
+		StdDraw.filledCircle(x, y, radiusYellow);
+		
+        StdDraw.show();
 
 		
 	}
@@ -62,6 +79,17 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
+		for (int i = 0; i<source.length(); i++) {
+			if (source.charAt(i) == target) {
+				result +=  replacement; 
+			}
+				else {
+					result += source.charAt(i);
+			}
+			
+		}
+				
+		
 		
 		return result;
 	}
@@ -75,8 +103,12 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
+		for (int i = 0; i<values.length; i++) {
+			sum += values.length;
 		
+		}
 		return sum;
+		
 	}
 
 	/**
